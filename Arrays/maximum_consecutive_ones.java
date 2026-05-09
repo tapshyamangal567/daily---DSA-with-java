21bf1fe385d7d7ccc87e94a8b80e252268b7aa90
Tapshya Mangal <mangaltapasya@gmail.com>
2026-05-09 20:41:27 +0530

    Add solution for maximum consecutive ones problem
    Implement a solution to find the maximum number of consecutive 1s in a binary array.

---
+++ Arrays/maximum_consecutive_ones.java
@@ -0,0 +1,22 @@
+// Problem : Given a binary array nums, return the maximum number of consecutive 1s in the array.
+// Platform : takeuforward
+// Difficulty : easy
+// TC  : O(N)
+// SC : O(1)
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count =0;
+        int max_count =0;
+        for(int i=0;i<nums.length;i++) {
+            if(nums[i] == 1) {
+                count++;
+                max_count = Math.max(max_count,count);
+            }
+            else {
+                count =0;
+            }
+        }
+        return max_count;
+    }
+}
